484554f5b1aae10f19aa42a6c7d697c9d1fa2026
Kashofi2010 <kashofi2010@gmail.com>
2026-05-21 20:07:01 -0500

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,13 @@
+#Input sirve para que el usuario escriba, en los parentesis es el mensaje que le va a aparecer
+nombre =input(" Escribe tu Nombre")
+
+#La f sirve para tomar el dato nombre tal cual se escriba 
+Saludo = f"Bienvenido {nombre } Con este programa sabrás tu nombre de superheroe"
+
+#Con print hacemos que este mensaje se envie
+print (Saludo)
+
+Color=input("¿Cuál es tu color favorito?")
+Animal=input("¿Cuál es tu animal favorito?")
+print("tu nombre es: El" + Color +""+ Animal)
+#Print suma los dos datos y da el mensaje 
